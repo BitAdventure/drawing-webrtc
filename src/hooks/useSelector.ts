67c9b30885aa05afaff7034098a386b4617c97ed
ssfmsfm <threeels.7@gmail.com>
2025-02-13 18:12:50 +0300

    remove unnecessary file

--- src/hooks/useSelector.ts
+++
@@ -1,6 +0,0 @@
-import { TypedUseSelectorHook, useSelector as useNativeSelector } from "react-redux";
-import store from "./../store/store";
-
-export const useSelector: TypedUseSelectorHook<ReturnType<typeof store.getState>> = useNativeSelector;
-
-export type RootState = ReturnType<typeof store.getState>;
